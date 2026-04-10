d44aa609d7bd5ead4b0b8f6520eb3a17ec00a8af
Arth <arthsalgia@gmail.com>
2026-04-10 17:20:13 -0400

    feature: rules button

--- frontend/codenames-app/src/components/rules.jsx
+++ frontend/codenames-app/src/components/rules.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './rules.css'
+import RulesText from './rulesText';
 
 export default function Rules() {
   const [showRules, setShowRules] = useState(false);
@@ -13,12 +14,8 @@ export default function Rules() {
       {showRules && (
         <div className="rules-overlay" onClick={() => setShowRules(false)}>
           <div className="rules-popup" onClick={e => e.stopPropagation()}>
-            <h2>Rules</h2>
-            <p>The goal of the game is simple: start a new round and try to achieve the highest score possible by making the correct choices at each step.
-              As the game progresses, the difficulty increases, requiring quicker thinking and better decisions.
-              Each correct action earns you points, while mistakes may cost you progress or end the round.
-              Stay focused, learn from each attempt, and aim to improve your performance every time you play.
-            </p>
+            <RulesText/>
+
             <button onClick={() => setShowRules(false)}>X</button>
           </div>
         </div>
