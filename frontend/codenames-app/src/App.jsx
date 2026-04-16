9ba13c74aaf7b3ebd60a4653d925b56e1fe2fbce
Arth <arthsalgia@gmail.com>
2026-04-15 21:26:34 -0400

    feature: 404 page

--- frontend/codenames-app/src/App.jsx
+++ frontend/codenames-app/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import StartGame from './pages/startGame.jsx';
 import Game from './pages/game.jsx'
 import Header from './components/header/header.jsx'
 import Home from './pages/home.jsx'
-import JoinGame from './pages/joinGame.jsx';
-import HostGame from './pages/hostGame.jsx';
+import JoinGame from './pages/Game/joinGame.jsx';
+import HostGame from './pages/Game/hostGame.jsx';
+import NotFound from './pages/notFound.jsx';
 import './App.css';
 
 export default function App() {
@@ -17,6 +17,7 @@ export default function App() {
           <Route path="/play-game" element={<Game />} />
           <Route path="/host-game" element={<HostGame />} />
           <Route path="/join-game" element={<JoinGame />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </>
