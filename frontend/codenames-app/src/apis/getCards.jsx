0b6aacf0d3f6c6b1928f91ba061b3f90c7004557
Arth <arthsalgia@gmail.com>
2026-04-19 01:50:12 -0400

    fix: getCards api

--- frontend/codenames-app/src/apis/getCards.jsx
+++ frontend/codenames-app/src/apis/getCards.jsx
@@ -1,15 +1,14 @@
-import { useParams } from "react-router-dom";
-
-export default async function getCards() {
-  const { gameId } = useParams();
-  const res = await fetch(`https://arthsalgia-codenames.onrender.com/get-cards?game_id=${gameId}`, {
-    method: "GET",
-  });
+export default async function getCards(gameId) {
+  const res = await fetch(
+    `https://arthsalgia-codenames.onrender.com/get-players?game_id=${gameId}`,
+    {
+      method: "GET",
+    }
+  );
 
   if (!res.ok) {
-    throw new Error("Failed to fetch cards");
+    throw new Error("Failed to fetch Cards");
   }
 
-  const data = await res.json();
-  return data
+  return await res.json();
 }
