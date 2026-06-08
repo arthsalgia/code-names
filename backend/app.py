b2285d9441dfeab3148ac56e24d5600b2d688328
Arth <arthsalgia@gmail.com>
2026-06-08 17:35:43 +0530

    update CORS to allow vercel to connect

--- backend/app.py
+++ backend/app.py
@@ -23,6 +23,7 @@ app = FastAPI(lifespan=lifespan, title="Codenames API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://arthsalgia-codenames.vercel.app/",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
